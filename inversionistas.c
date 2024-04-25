bd4954d45cb1bed82d2647c8fe0ccaffed3c1bd1
Fernanda Lobo Serra <maria.lobo@alu.inspt.utn.edu.ar>
2024-04-25 17:50:06 -0300

    Inversionistas v1.1

--- inversionistas.c
+++ inversionistas.c
@@ -9,15 +9,15 @@ int main(void)
     
     printf("Ingrese el nombre del primer inversionista:\n");
     scanf("%s", nom1);
-    printf("Ingrese el dinero aportado por %s:\n$", nom1);
+    printf("\nIngrese el dinero aportado por %s:\n$", nom1);
     scanf("%f", &inv1);
-    printf("Ingrese el nombre del segundo inversionista:\n");
+    printf("\nIngrese el nombre del segundo inversionista:\n");
     scanf("%s", nom2);
-    printf("Ingrese el dinero aportado por %s:\n$", nom2);
+    printf("\nIngrese el dinero aportado por %s:\n$", nom2);
     scanf("%f", &inv2);
-    printf("Ingrese el nombre del tercer inversionista:\n");
+    printf("\nIngrese el nombre del tercer inversionista:\n");
     scanf("%s", nom3);
-    printf("Ingrese el dinero aportado por %s:\n$", nom3);
+    printf("\nIngrese el dinero aportado por %s:\n$", nom3);
     scanf("%f", &inv3);
 
     total = inv1 + inv2 + inv3;
@@ -26,7 +26,7 @@ int main(void)
     por2 = inv2 * 100 / total;
     por3 = inv3 * 100 / total;
 
-    printf("El total del dinero invertido es $%f\n\n", total);
+    printf("\n\nEl total del dinero invertido es $%f\n\n", total);
     printf("La inversion de %s es de $%f lo que representa un %f%c del total\n\n", nom1, inv1, por1, porc);
     printf("La inversion de %s es de $%f lo que representa un %f%c del total\n\n", nom2, inv2, por2, porc); 
     printf("La inversion de %s es de $%f lo que representa un %f%c del total\n\n", nom3, inv3, por3, porc); 
